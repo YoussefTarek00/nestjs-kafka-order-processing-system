a572a40ac7127552b90634ee127544dd9f94fd0c
Youssef Tarek <ytarek.dev@gmail.com>
2026-06-02 19:33:52 +0300

    Add CorrelationIdInterceptor to handle correlation IDs in HTTP requests

---
+++ orders-service/src/common/interceptors/correlation-id.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class CorrelationIdInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    if (context.getType() !== 'http') {
+      return next.handle();
+    }
+
+    const request = context.switchToHttp().getRequest();
+    request.correlationId =
+      request.headers['x-correlation-id'] || uuidv4();
+
+    return next.handle();
+  }
+}
